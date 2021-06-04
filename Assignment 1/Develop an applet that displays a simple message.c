9ddc43b039aac12b8f2cda8f03518a8400ae574a
shahriea901 <84613349+shahriea901@users.noreply.github.com>
2021-06-04 13:03:32 +0600

    Add files via upload

---
+++ Assignment 1/Develop an applet that displays a simple message.c
@@ -0,0 +1,15 @@
+import java.applet.*;
+import java.awt.*;
+public class AppletDemo extends Applet
+{
+public void init()
+{
+setBackground(Color.red);
+}
+public void paint(Graphics g)
+{
+g.setColor(Color.blue);
+g.drawString(“SREE DATHA INSTITUTE OF TECHNOLOGY AND SCIENCE “,100,100);
+}
+}
+
